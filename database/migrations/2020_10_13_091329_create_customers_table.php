3fbdee7a952b3c0c676722c9455b5773afb43a9c
Manh Huynh <huynhga198@gmail.com>
2020-10-15 09:37:20 +0700

    them sua xoa timkiem mail

--- database/migrations/2020_10_13_091329_create_customers_table.php
+++ database/migrations/2020_10_13_091329_create_customers_table.php
@@ -15,6 +15,22 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('email')->unique()->nullable();
+            $table->string('dob')->nullable();
+            $table->string('address1');
+            $table->string('address2')->nullable();
+            $table->string('company')->nullable();
+            $table->string('city');
+            $table->string('state')->nullable();
+            $table->string('country');
+            $table->string('zip');
+            $table->string('phone');
+            $table->string('other_info')->nullable();
+            $table->string('note')->nullable();
+            $table->string('status')->nullable();
             $table->timestamps();
         });
     }
